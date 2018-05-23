90f18136450dd11f78b3f1b2fb96b1b9571a5cb5
Fernando Martin <femartin@itba.edu.ar>
2018-05-23 18:27:25 -0300

    Modo Video

--- 20 Domingo - Pure64/Kernel/kernel.c
+++ 20 Domingo - Pure64/Kernel/kernel.c
@@ -4,6 +4,7 @@
 #include <moduleLoader.h>
 #include <naiveConsole.h>
 #include <idtLoader.h>
+#include <stdint.h>
 
 
 extern uint8_t text;
@@ -81,12 +82,68 @@ void * initializeKernelBinary()
 	newLine();
 	return getStackBase();
 }
+struct vbe_mode_info_structure {
+	uint16_t attributes;		// deprecated, only bit 7 should be of interest to you, and it indicates the mode supports a linear frame buffer.
+	uint8_t window_a;			// deprecated
+	uint8_t window_b;			// deprecated
+	uint16_t granularity;		// deprecated; used while calculating bank numbers
+	uint16_t window_size;
+	uint16_t segment_a;
+	uint16_t segment_b;
+	uint32_t win_func_ptr;		// deprecated; used to switch banks from protected mode without returning to real mode
+	uint16_t pitch;			// number of bytes per horizontal line
+	uint16_t width;			// width in pixels
+	uint16_t height;			// height in pixels
+	uint8_t w_char;			// unused...
+	uint8_t y_char;			// ...
+	uint8_t planes;
+	uint8_t bpp;			// bits per pixel in this mode
+	uint8_t banks;			// deprecated; total number of banks in this mode
+	uint8_t memory_model;
+	uint8_t bank_size;		// deprecated; size of a bank, almost always 64 KB but may be 16 KB...
+	uint8_t image_pages;
+	uint8_t reserved0;
+
+	uint8_t red_mask;
+	uint8_t red_position;
+	uint8_t green_mask;
+	uint8_t green_position;
+	uint8_t blue_mask;
+	uint8_t blue_position;
+	uint8_t reserved_mask;
+	uint8_t reserved_position;
+	uint8_t direct_color_attributes;
+
+	uint32_t framebuffer;		// physical address of the linear frame buffer; write here to draw to the screen
+	uint32_t off_screen_mem_off;
+	uint16_t off_screen_mem_size;	// size of memory in the framebuffer but not being displayed on the screen
+	uint8_t reserved1[206];
+} __attribute__ ((packed));
+
+struct vbe_mode_info_structure* screenData = 0x5C00;
+
+static void fillrect(uint32_t donde,unsigned char r, unsigned char g, unsigned   char b, unsigned char w, unsigned char h) {
+	unsigned char *where=donde;
+	int i, j;
+
+	for (i = 0; i < w; i++) {
+		for (j = 0; j < h; j++) {
+			//putpixel(vram, 64 + j, 64 + i, (r << 16) + (g << 8) + b);
+			where[j*4] = r;
+			where[j*4 + 1] = g;
+			where[j*4 + 2] = b;
+		}
+		where+=3200;
+	}
+}
 
 int main()
 {
 	clearScreen();
 	load_idt();
 	printCurrentTime();
+	fillrect(screenData->framebuffer,50,90,20,60,60) ;
+
 
 	//printHex(readKey());
 	//printCurrentTime();
